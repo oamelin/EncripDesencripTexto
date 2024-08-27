8f56291851e507a8d1743bb65cb52d1221234e3e
Oscar <melinoscar@gmail.com>
2024-08-27 15:27:31 -0300

    VersionFinal

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ function encriptar() {
     .replace(/u/gi, "ufat");
 
   if (texto.length != 0) {
-    document.getElementById("texto").value = textoCifrado;
+    document.getElementById("mensaje").value = textoCifrado;
     tituloMensaje.textContent = "Texto encriptado con éxito";
     parrafo.textContent = "";
-    muñeco.src = "./img/encriptado.jpg";
+    muñeco.src="";
+    /*muñeco.src = "./img/encriptado.jpg";*/
   } else {
     muñeco.src = "./img/muñeco.png";
     tituloMensaje.textContent = "Ningún mensaje fue encontrado";
@@ -38,14 +39,35 @@ function desencriptar() {
     .replace(/ufat/gi, "u");
   
     if (texto.length != 0) {
-      document.getElementById("texto").value = textoCifrado;
+      document.getElementById("mensaje").value = textoCifrado;
       tituloMensaje.textContent = "Texto desencriptado con éxito";
       parrafo.textContent = "";
-      muñeco.src = "./img/desencriptado.jpg";
+      muñeco.ATTRIBUTE_NODE.valueOf;
+      /*muñeco.src = "./img/desencriptado.jpg";*/
     } else {
       muñeco.src = "./img/muñeco.png";
       tituloMensaje.textContent = "Ningún mensaje fue encontrado";
       parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
       swal("Ooops!", "Debes ingresar un texto", "warning");
     }
+}
+
+function copiar(){
+  let texto = document.getElementById("texto").value;
+  let mensaje = document.getElementById("mensaje").value;
+  let tituloMensaje = document.getElementById("titulo-mensaje");
+  let parrafo = document.getElementById("parrafo");
+  let muñeco = document.getElementById("muñeco");
+
+    if (mensaje.length != 0) {
+      tituloMensaje.textContent = "Texto Copiado con éxito";
+      parrafo.textContent = "";
+      muñeco.src = "";
+    } else {
+      muñeco.src = "./img/muñeco.png";
+      tituloMensaje.textContent = "Ningún mensaje fue encontrado";
+      parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+    }
+  document.getElementById("texto").value = mensaje;
+  document.getElementById("mensaje").value="";
 }
